ab91606b4d3455b4d747fda3749244a17823a028
Scott T Weaver <scott.t.weaver@gmail.com>
2022-07-07 13:40:22 -0500

    Schema complete with a validation spec

--- src/test/scala/zio/zymposium/tfz/SchemaValidationSpec.scala
+++ src/test/scala/zio/zymposium/tfz/SchemaValidationSpec.scala
@@ -3,6 +3,8 @@ package zio.zymposium.tfz
 import zio.test._
 import io.github.scottweaver.zio.testcontainers.postgres._
 import io.github.scottweaver.zio.aspect.DbMigrationAspect
+import java.sql.Connection
+import zio._
 
 object SchemaValidationSpec extends ZIOSpecDefault {
 
@@ -10,12 +12,26 @@ object SchemaValidationSpec extends ZIOSpecDefault {
     suite("SchemaValidationSpec")(
       test("validate schema") {
 
-        assertTrue(true)
-   
+        def testSelect(conn: Connection) = ZIO.attempt {
+          val stmt = conn.createStatement()
+          val rs1  = stmt.executeQuery("SELECT * FROM wizard")
+          val rs2  = stmt.executeQuery("SELECT * FROM spell")
+          val rs3  = stmt.executeQuery("SELECT * FROM spell_book")
+          val rs4  = stmt.executeQuery("SELECT * FROM spell_book_cross_reference")
+
+          stmt.close()
+        }
+
+        for {
+          conn <- ZIO.service[Connection]
+          _    <- testSelect(conn)
+        } yield assertTrue(true)
+
       } @@ DbMigrationAspect.migrate()()
-    ).provideShared(
-      ZPostgreSQLContainer.Settings.default,
-      ZPostgreSQLContainer.live
     )
-  
+      .provideShared(
+        ZPostgreSQLContainer.Settings.default,
+        ZPostgreSQLContainer.live
+      )
+
 }
